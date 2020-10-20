4c1ddfaac3ed1b427d71ededa9ec80cbdc272772
JP <jpjones22@gmail.com>
2020-10-20 13:50:13 -0700

    Created Local Storage Functionality.  Added test links to HTML

--- js/scenario.js
+++ js/scenario.js
@@ -1,8 +1,8 @@
 'use strict';
 
-function rollDice(max){
-  return Math.floor(Math.random() * (max - 1 + 1) + 1);
-}
+// function rollDice(max){
+//   return Math.floor(Math.random() * (max - 1 + 1) + 1);
+// }
 
 //Global variables for each section
 var questStartSection = document.getElementById('questStart');
@@ -14,42 +14,45 @@ var questStartTextArray = ['this is text'];
 var roll20Result = 0;
 
 //Retrieve Character information from local storage
-var characterFromLocalStorage = localStorage.getItem('character');
-var parsedCharacter = JSON.parse(characterFromLocalStorage);
-//Re run character information through constructor
-var userCharacter = new Character(parsedCharacter[0]);
+// var characterFromLocalStorage = localStorage.getItem('character');
+// var parsedCharacter = JSON.parse(characterFromLocalStorage);
+// //Re run character information through constructor
+// var userCharacter = new Character(parsedCharacter[0]);
+
+// object name is 'player'
+retrieveCharacter();
 
 
 //calculates the users roll based on the randomly generated number
-function calcRoll(stat, dieRoll, prof){ //stat: array index of the modifier we need, die: max for rollDice function, prof: name of skill used
+function calcRoll(stat, dieRoll, prof) { //stat: array index of the modifier we need, die: max for rollDice function, prof: name of skill used
   var baseRoll = dieRoll;
   var withMod = baseRoll + userCharacter.modArray[stat];
 
-  for(var i = 0; i < userCharacter.proficiencyArray.length; i++){
-    if(prof === userCharacter.proficiencyArray[i]){
+  for (var i = 0; i < userCharacter.proficiencyArray.length; i++) {
+    if (prof === userCharacter.proficiencyArray[i]) {
       var profBool = true;
     }
   }
-  if(profBool === true){
+  if (profBool === true) {
     var finalRoll = withMod + 2;
-  }else{
+  } else {
     finalRoll = withMod;
   }
   return finalRoll;
 }
 
 //event listeners
-document.getElementById('D20').addEventListener('click', handleD20);
-document.getElementById('D10').addEventListener('click', handleD10);
-document.getElementById('D8').addEventListener('click', handleD8);
+// document.getElementById('D20').addEventListener('click', handleD20);
+// document.getElementById('D10').addEventListener('click', handleD10);
+// document.getElementById('D8').addEventListener('click', handleD8);
 
 //renders the entire questStart section
-function renderQuestStart(){
+// function renderQuestStart(){
 
-}
+// }
 
 // renders the DM box div of the quest start section
-function renderQuestStartDMBox(){
+function renderQuestStartDMBox() {
   //tracks how many lines of dialogue have been displayed
   var buttonClickTracker = 0;
   //grabs section and appends p element
@@ -63,9 +66,15 @@ function renderQuestStartDMBox(){
   buttonClickTracker++;
 }
 
-function handleD20(){
+function handleD20() {
   var randomNumber = rollDice(20);
   roll20Result = randomNumber;
 }
+
+/// Execution Code <<--Gnarly-->>
+
+console.log('Roll Dice from app.js: ' + rollDice(10));
+console.log('in scenario player: ' + player);
+
 
 
