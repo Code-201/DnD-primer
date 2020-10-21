e69cbf441d4e60f567fd6bfa72f60bdbfc97fb85
MARIKO ALVARADO <marikoaalvarado@gmail.com>
2020-10-20 23:19:53 -0700

    open so far

--- js/open.js
+++ js/open.js
@@ -3,13 +3,25 @@
 //variable for easy access of each section:
 var statsSection = document.getElementById('stats');
 var diceSection = document.getElementById('diceRoll');
+var dmSection = document.getElementById('dmBox');
 
 var player;
 
 //Event Listeners:
-document.getElementById('rollD20').addEventListener('click', diceRoll);
-document.getElementById('rollD10').addEventListener('click', diceRoll);
-document.getElementById('rollD8').addEventListener('click', diceRoll);
+document.getElementById('rollD20').addEventListener('click', function () {
+  diceRoll(20);
+});
+document.getElementById('rollD10').addEventListener('click', function () {
+  diceRoll(10);
+});
+document.getElementById('rollD8').addEventListener('click', function () {
+  diceRoll(8);
+});
+document.getElementById('nextButton').addEventListener('click', renderDmSection);
+document.getElementById('nextButton').addEventListener('click', renderUserSection);
+
+document.getElementById('button').addEventListener('click',);
+
 
 
 retrieveCharacter(); //SOF (save our function :( )
@@ -17,7 +29,7 @@ retrieveCharacter(); //SOF (save our function :( )
 //renders the player stats portion of the window
 function renderStatsSection() {
   var nameTag = document.createElement('p');
-  nameTag.textContent = player.name;
+  nameTag.textContent = 'Name: ' + player.name;
   statsSection.appendChild(nameTag);
 
   var raceTag = document.createElement('p');
@@ -25,27 +37,63 @@ function renderStatsSection() {
   statsSection.appendChild(raceTag);
 
   var hitTag = document.createElement('p');
-  hitTag.textContent = player.hitPoints;
+  hitTag.textContent = 'Hit Points: ' + player.hitPoints;
   statsSection.appendChild(hitTag);
 
   var ulElement = document.createElement('ul');
+  var statNames = ['STR: ', 'DEX: ', 'CON: ', 'INT: ', 'WIS: ', 'CHA: ']
   statsSection.appendChild(ulElement);
   for (var i = 0; i < player.statArray.length; i++) {
     var liElement = document.createElement('li');
-    liElement.textContent = player.statArray[i];
+    liElement.textContent = statNames[i] + player.statArray[i];
     ulElement.appendChild(liElement);
   }
 
   var goldTag = document.createElement('p');
-  goldTag.textContent = player.equipment;
+  goldTag.textContent = 'Gold: ' + player.equipment;
   statsSection.appendChild(goldTag);
 }
 
-function diceRoll(sides){
-  var resultContainer = document.createElement('p');
+function diceRoll(sides) {
+  var resultContainer = document.getElementById('displayResult');
   var randomNum = diceValue(sides);
   resultContainer.textContent = `D${sides} rolled a ${randomNum}`;
-  diceSection.appendChild(resultContainer);
+
 }
 
+
+var nextClick = 0;
+function renderDmSection() {
+  var dmDialogue = ['dia 1 ', 'dia 2 ', 'dia 3 '];
+  var scenario = document.getElementById('displayDmDialogue');
+  scenario.textContent = dmDialogue[nextClick];
+  nextClick++;
+
+}
+
+var nextNext = 0;
+function renderUserSection() {
+  var usersDialogue = ['user 1 ', 'user 2 ', 'user 3 '];
+  var userPrompt = document.getElementById('displayUserDialogue');
+  userPrompt.textContent = usersDialogue[nextNext];
+  nextNext++;
+
+
+  if (nextNext === usersDialogue.length) {
+    document.getElementById('nextButton').removeEventListener('click', renderDmSection);
+    document.getElementById('nextButton').removeEventListener('click', renderUserSection);
+    document.getElementById('nextButtonContainer').innerHTML = '';
+    displayNextPageButton();
+  }
+}
+function displayNextPageButton() {
+  var nextPageContainer = document.getElementById('nextPageButtonContainer');
+  var nextPageButton = document.createElement('button');
+  nextPageButton.textContent = 'Go to the Blacksmith!';
+  nextPageContainer.appendChild(nextPageButton);
+}
+
+
 renderStatsSection();
+renderDmSection();
+renderUserSection();
