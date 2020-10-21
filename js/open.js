f65650d41be226ac3e77fff5077c547511024b60
overholtk <overholtkaya@gmail.com>
2020-10-21 02:06:15 -0700

    drying code

--- js/open.js
+++ js/open.js
@@ -20,40 +20,39 @@ document.getElementById('rollD8').addEventListener('click', function () {
 document.getElementById('nextButton').addEventListener('click', renderDmSection);
 document.getElementById('nextButton').addEventListener('click', renderUserSection);
 
-// document.getElementById('button').addEventListener('click',);
-
-
-
-retrieveCharacter(); //SOF (save our function :( )
+// retrieves character info from storage
+retrieveCharacter();
 
 //renders the player stats portion of the window
 function renderStatsSection() {
-  var nameTag = document.createElement('p');
-  nameTag.textContent = 'Name: ' + player.name;
-  statsSection.appendChild(nameTag);
+  renderPStats();
+  renderBaseStats();
+}
 
-  var raceTag = document.createElement('p');
-  raceTag.textContent = `Race: ${player.race}`;
-  statsSection.appendChild(raceTag);
+//Renders all stats connected to P elements
+function renderPStats(){
+  var statsNames = ['Name: ', 'Race: ', 'Hit Points: ', 'AC: ', 'Gold: '];
+  var statsValues = [player.name, player.race, player.hitPoints, player.armor, player.equipment];
+  for(var i =0; i < statsNames.length; i++){
+    var newP = document.createElement('p');
+    newP.textContent = statsNames[i] + statsValues[i];
+    statsSection.appendChild(newP);
+  }
+}
 
-  var hitTag = document.createElement('p');
-  hitTag.textContent = 'Hit Points: ' + player.hitPoints;
-  statsSection.appendChild(hitTag);
-
+//Renders all base stats connected to list items
+function renderBaseStats(){
   var ulElement = document.createElement('ul');
-  var statNames = ['STR: ', 'DEX: ', 'CON: ', 'INT: ', 'WIS: ', 'CHA: ']
+  var statNames = ['STR: ', 'DEX: ', 'CON: ', 'INT: ', 'WIS: ', 'CHA: '];
   statsSection.appendChild(ulElement);
   for (var i = 0; i < player.statArray.length; i++) {
     var liElement = document.createElement('li');
     liElement.textContent = statNames[i] + player.statArray[i];
     ulElement.appendChild(liElement);
   }
-
-  var goldTag = document.createElement('p');
-  goldTag.textContent = 'Gold: ' + player.equipment;
-  statsSection.appendChild(goldTag);
 }
 
+//gets base die Roll
 function diceRoll(sides) {
   var resultContainer = document.getElementById('displayResult');
   var randomNum = diceValue(sides);
@@ -61,7 +60,7 @@ function diceRoll(sides) {
 
 }
 
-
+//Renders dialogue for DM section
 var nextClick = 0;
 function renderDmSection() {
   var dmDialogue = ['dia 1 ', 'dia 2 ', 'dia 3 '];
@@ -71,6 +70,7 @@ function renderDmSection() {
 
 }
 
+//Renders dialogue for user section
 var nextNext = 0;
 function renderUserSection() {
   var usersDialogue = ['user 1 ', 'user 2 ', 'user 3 '];
@@ -80,12 +80,19 @@ function renderUserSection() {
 
 
   if (nextNext === usersDialogue.length) {
-    document.getElementById('nextButton').removeEventListener('click', renderDmSection);
-    document.getElementById('nextButton').removeEventListener('click', renderUserSection);
-    document.getElementById('nextButtonContainer').innerHTML = '';
-    displayNextPageButton();
+    endDialogue();
   }
 }
+
+//removes event listener and clears the next button from the page
+function endDialogue(){
+  document.getElementById('nextButton').removeEventListener('click', renderDmSection);
+  document.getElementById('nextButton').removeEventListener('click', renderUserSection);
+  document.getElementById('nextButtonContainer').innerHTML = '';
+  displayNextPageButton();
+}
+
+//displays the button that moves to the next page
 function displayNextPageButton() {
   var nextPageForm = document.getElementById('nextPageButtonContainer');
   var nextPageButton = document.createElement('input');
